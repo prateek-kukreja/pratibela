8e7eb69a74f9e66aad965c7a6f6b9d94fd14ee6d
Prateek <prateekkukreja@outlook.com>
2023-10-22 04:25:01 +0530

    popular article UI

--- src/components/popularArticle/PopularArticle.jsx
+++ src/components/popularArticle/PopularArticle.jsx
@@ -1,6 +1,7 @@
 import "./style.scss";
 import Design1 from "../postcards/design1/Design1Postcard";
 import DilliODilli from "../../images/blog/dilli-o-dilli.png";
+import { Link } from "react-router-dom";
 
 function PopularArticles() {
   return (
@@ -8,11 +9,19 @@ function PopularArticles() {
       <section className="popular-articles-section">
         <div className="container">
           <div className="popular-articles-content">
+            <h1>popular articles</h1>
+            <p>
+              Read more posts &nbsp;
+              <Link onClick={() => window.scrollTo(0, 0)} to="">
+                on the blog →
+              </Link>
+            </p>
             <Design1
               img={DilliODilli}
               title="Dilli-O-Dilli"
               text="Dilli sheher ki baat hi kuch aur hai! Delhi has always been portrayed very beautifully in cinema and books.."
             />
+            <div className="divider"></div>
           </div>
         </div>
       </section>
