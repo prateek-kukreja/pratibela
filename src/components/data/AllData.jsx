3cf7f4ef3880d2e17ec07994376bcf4e9495e325
Prateek <prateekkukreja@outlook.com>
2023-10-23 03:10:14 +0530

    blog page full UI, small fixes

--- src/components/data/AllData.jsx
+++ src/components/data/AllData.jsx
@@ -1,19 +1,50 @@
 import OldLadyHands from "../../images/blog/metabolism/old-lady-hands.png";
 import LampPhoto from "../../images/blog/inspiration/lamp-photo.png";
+import ThatsLife from "../../images/blog/thats-life/life.png";
+import Beach from "../../images/blog/life-by-the-sea/beach.png";
+import Black from "../../images/blog/brothel/black-image.png";
+import Book from "../../images/blog/loversbook/book.png";
 
 export const Blogs = [
   {
     id: 5,
     img: OldLadyHands,
     title: "How do some people consume so little?",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quo aliquam illum quae sapiente ratione ullam quisquam est quidem. Magnam?",
+    text: "How do some people simply breathe so little air? How their existence in the room they are breathing is very negligible? How some..",
     link: "how-do-some-people-consume-so-little?",
   },
   {
     id: 6,
     img: LampPhoto,
     title: "What’s calling me to write?",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quo aliquam illum quae sapiente ratione ullam quisquam est quidem. Magnam?",
+    text: "All of it had started from writing paragraphs on different subjects ranging from, “if I were a peacock” to “if I were the prime minister”..",
     link: "whats-calling-me-to-write?",
+  },
+  {
+    id: 7,
+    img: ThatsLife,
+    title: "That’s life",
+    text: "I sit in the good daylight, and the pattern of the window panes reflect the lightest oil pastel shade of pink which I suppose I should google..",
+    link: "how-do-some-people-consume-so-little?",
+  },
+  {
+    id: 8,
+    img: Beach,
+    title: "Life by the sea",
+    text: "I wish I could live by the sea, or some days I wish I could just live in my apartment and the windows open to the sea. They are different..",
+    link: "whats-calling-me-to-write?",
+  },
+  {
+    id: 9,
+    img: Black,
+    title: "A WOMAN IN BROTHEL.",
+    text: "Reena was married to Rajesh when she was twenty-two. “Raena”, the only son, is 9 years old and is a simple boy. They were the people of..",
+    link: "whats-calling-me-to-write?",
+  },
+  {
+    id: 10,
+    img: Book,
+    title: "Extracts from a lover’s notebook",
+    text: "My letter is as middle class as I am. I will try to talk about stars and moon and galaxies, but eventually, it will end on things that I deserve. Dear… ",
   },
 ];
